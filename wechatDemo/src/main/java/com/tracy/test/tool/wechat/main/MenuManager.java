4449cf446ada874a6ebe04390280068eadc0190d
tracyxiang-tx <tracy.xiang@missionsky.com>
2017-06-11 15:17:25 +0800

    完善部分自定义公众号菜单功能

--- wechatDemo/src/main/java/com/tracy/test/tool/wechat/main/MenuManager.java
+++ wechatDemo/src/main/java/com/tracy/test/tool/wechat/main/MenuManager.java
@@ -55,62 +55,70 @@ public class MenuManager {
      */
     private static Menu getMenu() {
         CommonButton btn11 = new CommonButton();
-        btn11.setName("天气预报");
-        btn11.setType("click");
-        btn11.setKey("11");
+        btn11.setName("打开链接");
+        btn11.setType("view");
+        btn11.setUrl("http://www.runoob.com");
 
         CommonButton btn12 = new CommonButton();
-        btn12.setName("公交查询");
+        btn12.setName("点击回复");
         btn12.setType("click");
         btn12.setKey("12");
-
+        
         CommonButton btn13 = new CommonButton();
-        btn13.setName("周边搜索");
-        btn13.setType("click");
+        btn13.setName("扫码");
+        btn13.setType("scancode_push");
         btn13.setKey("13");
-
+        
         CommonButton btn14 = new CommonButton();
-        btn14.setName("历史上的今天");
-        btn14.setType("click");
+        btn14.setName("扫码回信息");
+        btn14.setType("scancode_waitmsg");
         btn14.setKey("14");
+        
+        CommonButton btn15 = new CommonButton();
+        btn15.setName("发送位置");
+        btn15.setType("location_select");
+        btn15.setKey("15");
+        
 
         CommonButton btn21 = new CommonButton();
-        btn21.setName("歌曲点播");
-        btn21.setType("click");
+        btn21.setName("系统拍照发图");
+        btn21.setType("pic_photo_or_album");
         btn21.setKey("21");
 
         CommonButton btn22 = new CommonButton();
-        btn22.setName("经典游戏");
-        btn22.setType("click");
+        btn22.setName("微信相册发图");
+        btn22.setType("pic_weixin");
         btn22.setKey("22");
-
+        
         CommonButton btn23 = new CommonButton();
-        btn23.setName("美女电台");
-        btn23.setType("click");
+        btn23.setName("拍照相册发图");
+        btn23.setType("pic_sysphoto");
         btn23.setKey("23");
 
-        CommonButton btn24 = new CommonButton();
-        btn24.setName("人脸识别");
-        btn24.setType("click");
-        btn24.setKey("24");
-
+        
+        //需要在微信公众平台配置素材获得素材的media_id
+       /* CommonButton btn24 = new CommonButton();
+        btn24.setName("发消息除文本");
+        btn24.setType("media_id");
+        btn24.setMediaId("24");
+        
         CommonButton btn25 = new CommonButton();
-        btn25.setName("聊天唠嗑");
-        btn25.setType("click");
-        btn25.setKey("25");
-
+        btn25.setName("图文消息");
+        btn25.setType("view_limited");
+        btn25.setMediaId("25");*/
+        
         CommonButton btn31 = new CommonButton();
-        btn31.setName("Q友圈");
+        btn31.setName("vae+");
         btn31.setType("click");
         btn31.setKey("31");
 
         CommonButton btn32 = new CommonButton();
-        btn32.setName("电影排行榜");
+        btn32.setName("许嵩官方论坛");
         btn32.setType("click");
         btn32.setKey("32");
 
         CommonButton btn33 = new CommonButton();
-        btn33.setName("幽默笑话");
+        btn33.setName("许嵩百度图片");
         btn33.setType("click");
         btn33.setKey("33");
 
@@ -120,18 +128,18 @@ public class MenuManager {
          */
         
         ComplexButton mainBtn1 = new ComplexButton();
-        mainBtn1.setName("生活助手");
+        mainBtn1.setName("测试1");
         //一级下有4个子菜单
-        mainBtn1.setSub_button(new CommonButton[] { btn11, btn12, btn13, btn14 });
+        mainBtn1.setSub_button(new CommonButton[] { btn11, btn12, btn13, btn14, btn15 });
 
         
         ComplexButton mainBtn2 = new ComplexButton();
-        mainBtn2.setName("休闲驿站");
-        mainBtn2.setSub_button(new CommonButton[] { btn21, btn22, btn23, btn24, btn25 });
+        mainBtn2.setName("发图功能");
+        mainBtn2.setSub_button(new CommonButton[] { btn21, btn22, btn23 });
 
         
         ComplexButton mainBtn3 = new ComplexButton();
-        mainBtn3.setName("更多体验");
+        mainBtn3.setName("许嵩相关");
         mainBtn3.setSub_button(new CommonButton[] { btn31, btn32, btn33 });
 
         
